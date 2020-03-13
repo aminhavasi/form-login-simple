b8f3877ebf78074893ff51c88d38a11cc735a728
amin havasi <pau.ahq@gmail.com>
2020-03-13 04:31:22 +0330

    changess

--- src/app.js
+++ src/app.js
@@ -4,45 +4,45 @@ class App extends Component {
     render() {
         return (
             <div className="container">
-                <form id="form " className="form">
-                    <h2>Register form</h2>
+                <form id="form" className="form">
+                    <h2>Register With Us</h2>
                     <div className="form-control">
-                        <label htmlFor="username"> Username</label>
+                        <label htmlFor="username">Username</label>
                         <input
                             type="text"
                             id="username"
-                            placeholder="Enter username..."
+                            placeholder="Enter username"
                         />
                         <small>Error message</small>
                     </div>
                     <div className="form-control">
-                        <label htmlFor="username"> Email</label>
+                        <label htmlFor="email">Email</label>
                         <input
-                            type="email"
+                            type="text"
                             id="email"
-                            placeholder="Enter email..."
+                            placeholder="Enter email"
                         />
                         <small>Error message</small>
                     </div>
                     <div className="form-control">
-                        <label htmlFor="username"> password</label>
+                        <label htmlFor="password">Password</label>
                         <input
                             type="password"
                             id="password"
-                            placeholder="Enter password..."
+                            placeholder="Enter password"
                         />
                         <small>Error message</small>
                     </div>
                     <div className="form-control">
-                        <label htmlFor="username"> re-password</label>
+                        <label htmlFor="password2">Confirm Password</label>
                         <input
                             type="password"
-                            id="repassword"
-                            placeholder="Enter re-password..."
+                            id="password2"
+                            placeholder="Enter password again"
                         />
                         <small>Error message</small>
                     </div>
-                    <button type="submit">submit</button>
+                    <button type="submit">Submit</button>
                 </form>
             </div>
         );
